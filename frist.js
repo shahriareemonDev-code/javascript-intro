06a97b6c84b2e6e5a0dfc2997e8a3b84ccbb29e2
Shariare Emon <Shahriareemon2580@gmail.com>
2026-01-26 17:19:21 +0600

    javascript basic intro

---
+++ frist.js
@@ -0,0 +1,11 @@
+var bangla = 70;
+var eng = 80;
+var math = 80;
+var phycics = 70;
+var higherMath = 89;
+
+var totalSubject = 5;
+const total = bangla + eng + math + phycics + higherMath;
+console.log(total);
+var average = total / totalSubject;
+console.log(average);
